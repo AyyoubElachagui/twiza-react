01fd1ae78930f391c56319d05f3260b46af08908
Ayyoub elachagui <best_way_development@outlook.fr>
2025-12-09 19:10:34 +0100

    initial

--- src/components/alert/CustomAlert.js
+++ src/components/alert/CustomAlert.js
@@ -4,6 +4,7 @@ import { Alert } from '@mui/material'
 import {useTranslation} from "react-i18next";
 
 const CustomAlert = (props) => {
+    
     const { type, text } = props
     const {t} = useTranslation()
     return (
